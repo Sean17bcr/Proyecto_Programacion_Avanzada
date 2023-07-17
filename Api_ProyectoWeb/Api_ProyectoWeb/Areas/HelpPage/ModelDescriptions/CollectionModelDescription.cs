65e7f99cf721cb9c14612c9c2fdb844cbcf4e1e5
Sean17bcr <106638574+Sean17bcr@users.noreply.github.com>
2023-07-16 20:21:27 -0600

    Creacion del Api Web
    se creó el api en esta rama

---
+++ Api_ProyectoWeb/Api_ProyectoWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Api_ProyectoWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Api_ProyectoWeb/Api_ProyectoWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Api_ProyectoWeb.WebApiApplication" Language="C#" %>
